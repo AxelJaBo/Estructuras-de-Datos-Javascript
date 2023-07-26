f1e1f2df23f90e22e3b4f8bf4658a8fc2b866730
Daniel <axel.910921@gmail.com>
2023-07-25 20:45:33 -0700

    getAll and delete

--- hashTable/hashTable.js
+++ hashTable/hashTable.js
@@ -9,22 +9,49 @@ class HashTable {
         }
         return hash;
     }
-    set(key, value){
+    set(key, value) {
         const address = this.hashMethod(key);
-        if(!this.data[address]){
+        if (!this.data[address]) {
             this.data[address] = [];
         }
         this.data[address].push([key, value]);
         return this.data;
     }
 
-    get(key){
+    get(key) {
         const address = this.hashMethod(key);
         const currentBucket = this.data[address];
         if (currentBucket) {
             for (let i = 0; i < currentBucket.length; i++) {
                 if (currentBucket[i[0]] === key) {
                     return currentBucket[i][1];
+                }
+            }
+        }
+        return undefined;
+    }
+
+    getAllKeys() {
+        const keys = [];
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i]) {
+                for (let j = 0; j < this.data[i].length; j++) {
+                    keys.push(this.data[i][j][0]);
+                }
+            }
+        }
+        return keys;
+    }
+
+    remove(key) {
+        const address = this.hashMethod(key);
+        const currentBucket = this.data[address];
+        if (currentBucket) {
+            for (let i = 0; i < currentBucket.length; i++) {
+                if (currentBucket[i][0] === key) {
+                    const deletedValue = this.data[address][i];
+                    this.data[address].splice(i, 1);
+                    return deletedValue;
                 }
             }
         }
