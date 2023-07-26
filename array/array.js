b56a8a4ad18a0304877b5aa4b6fff172a22681d7
Daniel <axel.910921@gmail.com>
2023-07-25 20:00:48 -0700

    unshift

--- array/array.js
+++ array/array.js
@@ -1,40 +1,56 @@
 // const array = ["Diego", "Karen", "Oscar"];
 
 class MyArray {
-    constructor(){
+    constructor() {
         this.length = 0;
         this.data = {};
     }
 
-    get(index){
+    get(index) {
         return this.data[index];
     }
 
-    push(item){
+    push(item) {
         this.data[this.length] = item;
         this.length++;
         return this.data;
     }
 
-    pop(){
-        const lastItem = this.data[this.length-1];
-        delete this.data[this.length-1];
+    pop() {
+        const lastItem = this.data[this.length - 1];
+        delete this.data[this.length - 1];
         this.length--;
         return lastItem;
     }
 
-    delete(index){
+    delete(index) {
         const item = this.data[index];
         this.shiftIndex(index);
         return item;
     }
 
-    shiftIndex(index){
-        for(let i = index; i<this.length-1; i++){
-            this.data[i]=this.data[i+1];
+    shiftIndex(index) {
+        for (let i = index; i < this.length - 1; i++) {
+            this.data[i] = this.data[i + 1];
         }
-        delete this.data[this.length-1];
+        delete this.data[this.length - 1];
         this.length--;
+    }
+
+    unshift(item) {
+        // Tu código aquí 👈 
+        if (!item) {
+            return this.length
+        }
+        if (this.length !== 0) {
+            for (let i = this.length - 1; i >= 0; i--) {
+                this.data[i + 1] = this.data[i];
+            }
+        }
+
+        this.data[0] = item;
+        this.length++;
+        return this.length
     }
 }
 
